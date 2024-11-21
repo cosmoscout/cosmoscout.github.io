fd7f788324581aa577df0d9edd70306b5ecba621
Simon Schneegans <code@simonschneegans.de>
2024-11-21 13:02:39 +0100

    :tada: Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# cosmoscout.github.io
+# The Homepage of [CosmoScout VR](https://github.com/cosmoscout/cosmoscout-vr)
+
+> [!IMPORTANT]  
+> This page is currently under construction! It will receive more content and major updates.
+
+Feel free to improve the documentation by submitting a pull request to this repository.
+If you want to preview your changes locally, you only need to clone your fork and run the following commands:
+
+```bash
+npm install
+npm run dev
+```
+
+This will start a local development server at http://localhost:4321.
+
+## Documentation
+
+This page uses the [Starlight](https://starlight.astro.build/getting-started/) theme for the [Astro](https://docs.astro.build/en/getting-started/) framework to generate static HTML files.
+You can find some documentation about the frameworks by using these links.
---
+++ astro.config.mjs
@@ -0,0 +1,59 @@
+// @ts-check
+import starlight from '@astrojs/starlight';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://cosmoscout.github.io',
+  integrations: [
+    icon(),
+    starlight({
+      title: 'CosmoScout VR',
+      logo: {
+        src: './src/assets/cosmoscout.svg',
+        alt: 'CosmoScout VR Logo',
+      },
+      editLink: {
+        baseUrl: 'https://github.com/cosmoscout/cosmoscout.github.io/edit/main/',
+      },
+      social: {
+        github: 'https://github.com/cosmoscout/cosmoscout-vr',
+      },
+      sidebar: [
+        {
+          label: 'Getting Started',
+          autogenerate: { directory: 'getting-started' },
+        },
+        {
+          label: 'Getting Involved',
+          autogenerate: { directory: 'getting-involved' },
+        },
+        {
+          label: 'Plugins',
+          autogenerate: { directory: 'plugins' },
+        },
+        {
+          label: 'Issue Tracker 🗗',
+          link: 'https://github.com/cosmoscout/cosmoscout-vr/issues',
+          attrs: { target: '_blank' },
+        },
+        {
+          label: 'Changelog 🗗',
+          link: 'https://github.com/cosmoscout/cosmoscout-vr/blob/main/docs/changelog.md',
+          attrs: { target: '_blank' },
+        },
+        {
+          label: 'Code of Conduct 🗗',
+          link: 'https://github.com/cosmoscout/cosmoscout-vr/blob/main/docs/code_of_conduct.md',
+          attrs: { target: '_blank' },
+        },
+      ],
+      lastUpdated: true,
+      customCss: ['./src/styles/custom.css'],
+      components: {
+        Footer: './src/components/Footer.astro',
+      },
+    }),
+  ],
+});
